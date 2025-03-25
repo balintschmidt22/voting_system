974f9af1a3fe918eea1acdae7f627eb9460cb543
Schmidt Bálint Márk <frjr89@inf.elte.hu>
2025-03-25 01:35:19 +0100

    First commit

---
+++ VotingSystem.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace VotingSystem.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
